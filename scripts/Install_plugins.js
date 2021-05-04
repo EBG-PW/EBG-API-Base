675f660fc2e29c650d4e4bd3f1701d0009dc49ce
BolverBlitz <35345288+BolverBlitz@users.noreply.github.com>
2021-05-04 21:30:05 +0200

    Added plugin updater

--- scripts/Install_plugins.js
+++ scripts/Install_plugins.js
@@ -49,18 +49,48 @@ function installPlugin(PL_ID){
     fs.writeFile(`${rootPath}.env`, Vorlage, (err) => {if (err) console.log(err);
         console.log('[Installer] \x1b[32m[OK]\x1b[0m',`Added ${PluginsPath[PL_ID].env} to .env File.`)
     });
+}
 
+function updatePlugin(PL_ID){
+    if(!fs.existsSync(`${rootPath}data/${PluginsPath[PL_ID].Folder}`)) {
+        fs.mkdirSync(`${rootPath}data/${PluginsPath[PL_ID].Folder}`);
+        console.log('[Installer] \x1b[32m[OK]\x1b[0m',`Folder ${PluginsPath[PL_ID].Folder} was created.`)
+    }
+
+     PluginsPath[PL_ID].Files.map(File => {
+        const file = fs.createWriteStream(`${rootPath}/${File.Path}`);
+         https.get(`${File.File}`, response => {
+            let stream = response.pipe(file);
+
+            stream.on("finish", function() {
+                console.log('[Installer] \x1b[32m[OK]\x1b[0m',`Downloaded file ${File.Path}`)
+            });
+        });
+    });
+
+    child_process.execSync(`npm install ${PluginsPath[PL_ID].npm}`,{stdio:[0,1,2]});
 }
 
 /* Questions */
 async function Start() {
+    dowhat = await askQuestion("Do you want to install or update a Plugin?\n1: Install\n2: Update\n> ");
     plugin = await askQuestion("Please choose the plugin you want to install\n1: MSH-Config-Generator\n2: PPS (Pterodactyl-ServerStarter)\n> ");
-    if(!fs.existsSync(`${rootPath}/${PluginsPath[plugin].Files[0].Path}`)) {
-        installPlugin(plugin)
+    if(dowhat === "1"){
+        if(!fs.existsSync(`${rootPath}/${PluginsPath[plugin].Files[0].Path}`)) {
+            installPlugin(plugin)
+            console.log('[Installer] \x1b[31m[IO]\x1b[0m',`${PluginsPath[plugin].Done}`)
+        }else{
+            console.log('[Installer] \x1b[31m[IO]\x1b[0m',`This plugin is already installed!`)
+        }
+    }else if(dowhat === "2"){
+        if(!fs.existsSync(`${rootPath}/${PluginsPath[plugin].Files[0].Path}`)) {
+            console.log('[Installer] \x1b[31m[IO]\x1b[0m',`This plugin can't be updated, because its not intalled!`)
+        }else{
+            updatePlugin(plugin)
+        }
     }else{
-        console.log('[Installer] \x1b[31m[IO]\x1b[0m',`This plugin is already installed!`)
+        console.log('[Installer] \x1b[31m[IO]\x1b[0m',`Unexpected Input ${dowhat}!`)
     }
-    console.log('[Installer] \x1b[31m[IO]\x1b[0m',`${PluginsPath[plugin].Done}`)
 }
 
 /* Start Script */
